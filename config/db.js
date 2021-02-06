2bc8c98462b895cab7e55c65945249a3def7384a
David Rodriguez <david@bsdadm.com>
2021-02-06 13:19:04 -0500

    add database connection file

--- config/db.js
+++ config/db.js
@@ -1,1 +1,22 @@
-// TODO: Create and establish the Database connection code.
+require('dotenv').config();
+const mysql = require('mysql');
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const database = process.env.DB_NAME;
+const db = mysql.createConnection({
+  host,
+  user,
+  password,
+  database
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(`There was an error connecting to the database: ${err.stack}`);
+    return;
+  }
+  console.log(`Connected to the database: ${db.threadId}`);
+});
+
+module.exports = db;
